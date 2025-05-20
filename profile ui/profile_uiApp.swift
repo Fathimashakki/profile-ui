113940274e3a23578692587026e65dc826e09141
Fathima Shakki K C <fathimashakki21@gmail.com>
2025-05-20 15:35:08 +0530

    Initial Commit

---
+++ profile ui/profile_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_uiApp.swift
+//  profile ui
+//
+//  Created by Shaan on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct profile_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
